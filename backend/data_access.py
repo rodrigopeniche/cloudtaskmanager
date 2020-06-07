41406d8ae1dd26655c5491de160ed32831bd1095
Rodrigo <rodrigo@inspr.com>
2020-06-07 17:34:28 -0500

    added database connection closure function in data access layer

--- backend/data_access.py
+++ backend/data_access.py
@@ -4,14 +4,15 @@ def connect_to_db():
     conn = psycopg2.connect("host=host.docker.internal dbname=cloud_reminders user=postgresadmin password=admin123 port=32420")
     return conn
 
-def insert(data):
-    conn = connect_to_db()
-    cur = conn.cursor()
-    print(data[0])
-    print(data[1])
-    print(data[2])
-    cur.execute("INSERT INTO task (description, urgency, status) VALUES ('"  + data[0] + "',"  + data[1]  + ",'" + data[2] + "');")
+def close_db(conn, cur):
     conn.commit()
     cur.close()
     conn.close()
+
+
+def insert(data):
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO task (description, urgency, status) VALUES ('"  + data[0] + "',"  + data[1]  + ",'" + data[2] + "');")
+    close_db(conn, cur)
 
